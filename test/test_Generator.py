e46473ea45da04b7ab869e6de4964e1875f5f399
velramiir <justus.zeinert@student.hpi.de>
2020-07-17 12:43:24 +0200

    first test for initial block

---
+++ test/test_Generator.py
@@ -0,0 +1,10 @@
+import torch
+
+from Generator import GenInitialBlock
+
+
+def test_shape():
+    block = GenInitialBlock(latent_size=512)
+    x = torch.randn(1, 512)
+    y = block(x)
+    assert y.shape == (1, 512, 4, 4)
