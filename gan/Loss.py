6bc27f78e965b1fd47b4d76dfa1ea56957bc418c
velramiir <justus.zeinert@student.hpi.de>
2020-07-20 18:58:20 +0200

    better explain loss

--- gan/Loss.py
+++ gan/Loss.py
@@ -1,26 +1,29 @@
 import torch
 
 
-class Loss:
+class WassersteinLoss:
+    """
+    Implements the improved Wasserstein loss with gradient penalty (WGAN-GP) as suggested by Gulrajani et al.
+    (https://arxiv.org/pdf/1704.00028.pdf) and as used in the NVIDIA progressive gan paper. The original objective
+    function is described in equation 2 and the regularizing gradient penalty term in equation 3.
+    The Wasserstein loss encourages the critic / discriminator to separate output for real and fake images
+    respectively, i.e. large values for real images, small values for fakes. The generator is encouraged to produce
+    fake images with high scores (realistic ones).
+    This penalty is needed to keep the gradient lengths in a compact interval (Lipschitz constraint). This in turn is
+    a mathematical property required by the Wasserstein loss.
+    Additionally this class uses the penalty term with a small weight (eps_drift) for large critic / discriminator
+    output suggested in the progressive gan paper to keep output reasonably sized.
+    """
 
-    def __init__(self, discriminator):
+    def __init__(self, discriminator, eps_drift=0.001):
+        """
+        :param discriminator: discriminator module of the GAN being trained
+        :param eps_drift: weight for output size penalty
+        """
         self.discriminator = discriminator
+        self.eps_drift = eps_drift
 
-    def discriminator_loss(self, real_samples, fake_samples, current_depth, alpha):
-        raise NotImplementedError
-
-    def generator_loss(self, real_samples, fake_samples, current_depth, alpha):
-        raise NotImplementedError
-
-
-class WassersteinLoss(Loss):
-
-    def __init__(self, discriminator, drift=0.001, use_gradient_penalty=False):
-        super(WassersteinLoss, self).__init__(discriminator)
-        self.drift = drift
-        self.use_gradient_penalty = use_gradient_penalty
-
-    def __gradient_penalty(self, real_samples, fake_samples, current_depth, alpha, reg_lambda=10):
+    def __gradient_penalty(self, real_samples, fake_samples, current_depth, alpha, gp_lambda=10):
         batch_size = real_samples.shape[0]
 
         epsilon = torch.rand((batch_size, 1, 1, 1)).to(fake_samples.device)
@@ -38,17 +41,18 @@ class WassersteinLoss(Loss):
                                        only_inputs=True)[0]
         gradient = gradient.view(gradient.shape[0], -1)
 
-        return reg_lambda * torch.mean(gradient.norm(p=2, dim=1).sub(1).pow(2))
+        return gp_lambda * torch.mean(gradient.norm(p=2, dim=1).sub(1).pow(2))
 
     def discriminator_loss(self, real_samples, fake_samples, current_depth, alpha):
         output_for_fakes = self.discriminator(fake_samples, current_depth, alpha)
         output_for_real = self.discriminator(real_samples, current_depth, alpha)
 
-        loss = output_for_fakes.mean() - output_for_real.mean() + self.drift * output_for_real.pow(2).mean()
+        gradient_penalty = self.__gradient_penalty(real_samples, fake_samples, current_depth, alpha)
 
-        if self.use_gradient_penalty:
-            penalty = self.__gradient_penalty(real_samples, fake_samples, current_depth, alpha)
-            loss += penalty
+        # original Wasserstein loss (fake output counts positive to encourage minimization and vice versa)
+        loss = output_for_fakes.mean() - output_for_real.mean()
+        loss += gradient_penalty  # improved Wasserstein loss with gradient penalty
+        loss += self.eps_drift * output_for_real.pow(2).mean()  # drift constraint from progressive GAN
 
         return loss
 
